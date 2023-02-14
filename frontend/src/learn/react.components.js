7926861097ac24a3cbf95a3d56f76cc648d44865
JinYee Jeong <luvdo0112@daum.net>
2023-02-14 10:03:04 +0900

    0214

--- frontend/src/learn/react.components.js
+++ frontend/src/learn/react.components.js
@@ -2,14 +2,24 @@
 /* React Component Types                                                      */
 /* -------------------------------------------------------------------------- */
 
-// 1. class Component
+// component vs element
+// element -> <div></div>
+// componenet -> HTMLDivElemet class 
+
+// React 컴포넌트의 역할
+// -재사용
+// -React 엘리먼트
+//
+
+// 1. functional Component
+// 함수 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
+function React.Component() {
+  return React.createElement('div',{role:'group'}); //<div role="group"></div>
+
+}
+// 2. class Component
 // 클래스 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
 class {
 
 }
 
-// 2. functional Component
-// 함수 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
-function () {
-  
-}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,7 @@
 // React 컴포넌트 vs 엘리먼트
-import './learn/react.components.js';
+import { Group } from "./learn/react.components.js";
+
+// 함수 타입
+// React 월드
+// React 엘리먼트 생성 과정에서는 반드시 REact.createElement API
+console.log(React.createElement(Group));
