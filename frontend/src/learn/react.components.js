d6d61d4eca77d5585580b0ce3ddd05e325e6a92e
JinYee Jeong <luvdo0112@daum.net>
2023-02-14 10:51:39 +0900

    babel

--- frontend/src/learn/react.components.js
+++ frontend/src/learn/react.components.js
@@ -2,24 +2,23 @@
 /* React Component Types                                                      */
 /* -------------------------------------------------------------------------- */
 
-// component vs element
-// element -> <div></div>
-// componenet -> HTMLDivElemet class 
+// Component vs. Element
+// 엘리먼트 Element → <div></div>
+// 컴포넌트(구성요소) Component → HTMLDivElement class
 
 // React 컴포넌트의 역할
-// -재사용
-// -React 엘리먼트
-//
+// - 재사용
+// - React 엘리먼트 생성 (React.createElement)
+// - 생성된 React 엘리먼트 반환(return)
 
 // 1. functional Component
-// 함수 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
-function React.Component() {
-  return React.createElement('div',{role:'group'}); //<div role="group"></div>
-
+// 함수 문법을 사용해 React 요소를 여러 번 생성할 수 있는 컴포넌트 설계
+export function Group() {
+  return React.createElement("div", { role: "group" }); // <div role="group"></div>
 }
+
 // 2. class Component
-// 클래스 문법을 사용해 React 요소를 여러번 생성할 수 있는 컴포넌트 설계
-class {
+// 클래스 문법을 사용해 React 요소를 여러 번 생성할 수 있는 컴포넌트 설계
+// class {
 
-}
-
+// }
